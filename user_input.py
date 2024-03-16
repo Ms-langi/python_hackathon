20ac2088333930faccd70df0f3d0517284752f67
Ms-langi <159784563+Ms-langi@users.noreply.github.com>
2024-03-16 16:28:03 +0300

    deletion

--- user_input.py
+++ user_input.py
@@ -1,12 +1,1 @@
-#user_input.py
-# User's name
-name = input("what is your name?")
-# User's age
-age = input("How old are you?")
-# User's location
-location = input("Where are you located?")
-
-
-# Print
-print(f"Hello {name}, you are {age} years old and live in {location}.")
 
